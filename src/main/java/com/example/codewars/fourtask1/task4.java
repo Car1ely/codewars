13c97fd0ef0e7b91dbdb0d1de14c1d90f8d9a8ea
qwerty <arturl.lee@octo.uz>
2026-02-19 20:53:55 +0500

    fix part 1 (4 task)

--- src/main/java/com/example/codewars/fourtask1/task4.java
+++ src/main/java/com/example/codewars/fourtask1/task4.java
@@ -6,7 +6,7 @@ public class task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = 1;
-        while (sc.hasNext()) {
+        while (sc.hasNextLine()) {
             String line = sc.nextLine();
             System.out.println(i + " " + line);
             i++;
